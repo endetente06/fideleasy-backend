a062bfc53897e56b61b111b0733384ab0269d194
Daniel Fraier <daniel.fraier1@gmail.com>
2026-05-02 20:24:50 +0200

    debug

--- server.js
+++ server.js
@@ -165,9 +165,9 @@ app.get('/pass/apple/:card_id', async (req, res) => {
     const pass = await PKPass.from({
       model: '/app/passes/FidelEasy.pass',
       certificates: {
-     wwdr: Buffer.from(process.env.PASS_WWDR.replace(/\s/g, ''), 'base64'),
-signerCert: Buffer.from(process.env.PASS_CERT.replace(/\s/g, ''), 'base64'),
-signerKey: Buffer.from(process.env.PASS_KEY.replace(/\s/g, ''), 'base64'),
+     wwdr: Buffer.from(process.env.PASS_WWDR, 'base64'),
+signerCert: Buffer.from(process.env.PASS_CERT, 'base64'),
+signerKey: Buffer.from(process.env.PASS_KEY, 'base64'),
         signerKeyPassphrase: '123456'
       }
     }, {
