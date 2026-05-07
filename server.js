b06a6296259f00318d3686b0d03e907b26f2ac28
Daniel Fraier <daniel.fraier1@gmail.com>
2026-05-07 16:03:51 +0200

    EventTicket avec strip plein format

--- server.js
+++ server.js
@@ -255,21 +255,19 @@ console.log('card.shop_id:', card.shop_id);
     });
 
     // Modifier le pass.json avec les vraies données
-    const passJson = JSON.parse(fs.readFileSync(path.join(tmpDir, 'pass.json'), 'utf8'));
     passJson.serialNumber = `${card_id}_${Date.now()}`;
-passJson.storeCard.primaryFields[0].value = card.points.toString();
-passJson.storeCard.secondaryFields[0].value = `${card.stamps}/${shop?.card_stamps_required || 10}`;
-passJson.storeCard.auxiliaryFields[0].value = customer ? customer.name : 'Client';
-passJson.logoText = (shop?.card_logo_text || shop?.name || 'FidelEasy').substring(0, 20);
-passJson.backgroundColor = shop?.card_color || 'rgb(10, 10, 24)';
-passJson.foregroundColor = 'rgb(255, 255, 255)';
-passJson.labelColor = 'rgb(212, 175, 55)';
-console.log('backgroundColor appliqué:', passJson.backgroundColor);
-// Générer strip.png dynamique
+passJson.eventTicket.primaryFields[0].value = shop?.card_logo_text || shop?.name || 'FidelEasy';
+passJson.eventTicket.secondaryFields[0].value = `${card.stamps}/${shop?.card_stamps_required || 10}`;
+passJson.eventTicket.auxiliaryFields[0].value = customer ? customer.name : 'Client';
+passJson.logoText = '';
+passJson.backgroundColor = 'rgb(0,0,0)';
+passJson.foregroundColor = 'rgb(255,255,255)';
+passJson.labelColor = 'rgb(255,255,255)';
+
+// Strip plein format
 const stripBuffer = await generateStripImage(shop);
 fs.writeFileSync(path.join(tmpDir, 'strip.png'), stripBuffer);
 fs.writeFileSync(path.join(tmpDir, 'strip@2x.png'), stripBuffer);
-fs.writeFileSync(path.join(tmpDir, 'pass.json'), JSON.stringify(passJson));
 
     const pass = await PKPass.from({
       model: tmpDir,
